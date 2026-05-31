34cd31ef4c2636d3b7f4d18ea6e2fd6c588df8da
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2026-05-31 00:39:29 -0600

    feat: setup api client and lucide dependency

---
+++ src/services/api/models/internal_api_TelegramAuthRequest.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type internal_api_TelegramAuthRequest = {
+    auth_date?: number;
+    first_name?: string;
+    hash?: string;
+    id?: number;
+    photo_url?: string;
+    username?: string;
+};
+
---
+++ src/services/api/models/internal_api_UserResponse.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type internal_api_UserResponse = {
+    email?: string;
+    id?: number;
+    name?: string;
+};
+
